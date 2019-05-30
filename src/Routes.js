11c3d075d032a87f0a6e54cf507d3504f62b7cb9
Adebajo Oluwaseyi Omoniyi <adebajo.oluwaseyi@gmail.com>
2019-05-30 19:18:55 +0100

    #166357135 view meetup detail (#11)

--- src/Routes.js
+++ src/Routes.js
@@ -4,12 +4,14 @@ import Home from './pages/Home';
 import Login from './pages/Login';
 import Signup from './pages/Signup';
 import Meetups from './pages/users/Meetups';
+import Meetup from './pages/users/Meetup';
 
 const Routes = () => {
   return (
     <Switch>
       <Route path="/login" component={Login} />
       <Route path="/signup" component={Signup} />
+      <Route path="/meetups/:id" component={Meetup} />
       <Route path="/meetups" component={Meetups} />
       <Route path="/" component={Home} exact />
     </Switch>
